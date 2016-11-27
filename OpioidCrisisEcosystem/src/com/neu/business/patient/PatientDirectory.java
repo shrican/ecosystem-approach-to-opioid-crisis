7e8ab84c763889c31b8b265e3c0032e9bbfc183a
Soham Dasneogi <dasneogi.s@husky.neu.edu>
2016-11-27 16:00:16 -0500

    Adding Patient class skeleton

---
+++ OpioidCrisisEcosystem/src/com/neu/business/patient/PatientDirectory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.neu.business.patient;
+
+import java.util.List;
+
+/**
+ *
+ * @author Soham
+ */
+public class PatientDirectory {
+    
+    private List<Patient> patientList;
+}
