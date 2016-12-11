9a4f275ad93f3eccdbc12a59f5e29bf48a0db222
Anmol Bajaj <bajaj.an@husky.neu.edu>
2016-12-10 23:14:43 -0500

    changes

--- OpioidCrisisEcosystem/src/com/neu/userinterface/receptionistrole/PatientDetailsJPanel.java
+++ OpioidCrisisEcosystem/src/com/neu/userinterface/receptionistrole/PatientDetailsJPanel.java
@@ -211,7 +211,7 @@ public class PatientDetailsJPanel extends javax.swing.JPanel {
         userProcessContainer.remove(this);
         Component[] componentArray = userProcessContainer.getComponents();
         Component component = componentArray[componentArray.length - 1];
-        com.neu.userinterface.receptionistrole.ReceptionistWorkAreaJPanel receptionistwjp = (ReceptionistWorkAreaJPanel) component;
+        ReceptionistWorkAreaJPanel receptionistwjp = (ReceptionistWorkAreaJPanel) component;
         receptionistwjp.populateAppointentsTable(enterprise);
         CardLayout layout = (CardLayout) userProcessContainer.getLayout();
         layout.previous(userProcessContainer);
