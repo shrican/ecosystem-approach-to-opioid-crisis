1809df45471602f39b2b517073fac1c92ffc2258
Anmol Bajaj <bajaj.an@husky.neu.edu>
2016-12-11 14:09:54 -0500

    general symptoms breakdown

--- OpioidCrisisEcosystem/src/com/neu/business/enterprise/CommunityRehabEnterprise.java
+++ OpioidCrisisEcosystem/src/com/neu/business/enterprise/CommunityRehabEnterprise.java
@@ -23,6 +23,58 @@ public class CommunityRehabEnterprise extends Enterprise {
 
     }
     
+    public HashMap<String, Integer> generalSymptomsBreakdown(ArrayList<Patient> patientList)
+    {
+        int nauseaCount = 0;
+        int chestPainCount = 0;
+        int pupilaryConstrictionCount = 0;
+        int bloodShotEyesCount = 0;
+        int jointPainCount = 0;
+        int muscleTensionCount = 0;
+        int lowRespiratoryRateCount = 0;
+        int selfHarmCount = 0;
+        int insomniaCount = 0;
+        
+        for(Patient patient : patientList)
+        {
+            for(OpioidAbuseSymptoms abuseSymptoms : patient.getOpioidAbuseSymptomsHistory().getOpioidAbuseSysmpomsList())
+            {
+                if(abuseSymptoms.hasBloodshotEyes())
+                    bloodShotEyesCount++;
+                if(abuseSymptoms.hasChestPain())
+                    chestPainCount++;
+                if(abuseSymptoms.hasInsomnia())
+                    insomniaCount++;
+                if(abuseSymptoms.hasJointPain())
+                    jointPainCount++;
+                if(abuseSymptoms.hasLowRespiratoryRate())
+                    lowRespiratoryRateCount++;
+                if(abuseSymptoms.hasMuscleTension())
+                    muscleTensionCount++;
+                if(abuseSymptoms.hasNausea())
+                    nauseaCount++;
+                if(abuseSymptoms.hasPupilaryConstriction())
+                    pupilaryConstrictionCount++;
+                if(abuseSymptoms.hasSelfHarm())
+                    selfHarmCount++;
+            }
+        }
+        
+        HashMap<String, Integer> breakdown = new HashMap<>();
+        
+        breakdown.put("Nausea count", nauseaCount);
+        breakdown.put("Chest Pain count", chestPainCount);
+        breakdown.put("Pupilary Constriction count", pupilaryConstrictionCount);
+        breakdown.put("Bloodshot Eyes count", bloodShotEyesCount);
+        breakdown.put("Joint Pain count", jointPainCount);
+        breakdown.put("Muscle Tension count", muscleTensionCount);
+        breakdown.put("Low Respiratory Rate count", lowRespiratoryRateCount);
+        breakdown.put("Self Harm Count", selfHarmCount);
+        breakdown.put("Insomnia count", insomniaCount);
+        
+        return breakdown;
+    }
+    
     public void abuseSymptomsBreakdown(PatientDirectory patientDirectory)
     {
         //Breakdown of abuse symptoms for patients, which symptoms were seen how many times
